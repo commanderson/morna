887e5057fc471773a7960ccd81e0a05a24bb1ff2
Abhi Nellore <anellore@gmail.com>
2017-01-20 10:36:28 -0800

    adds command-line parameters to rnamore

---
+++ rnamore.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+"""
+rnamore
+
+Uses mmh3 to implement feature hashing for dimensionality reduction of 
+intropolis junction vectors by sample and uses Spotify's annoy library for
+obtaining nearest neighbors.
+
+Requires mmh3 (pip install mmh3) and Spotify's annoy (pip install annoy)
+"""
+import argparse
+from annoy import AnnoyIndex
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description=__doc__, 
+                formatter_class=argparse.RawDescriptionHelpFormatter)
+    # Add command-line arguments
+    parser.add_argument('--intropolis', type=str, required=False,
+            default=None,
+            help='path to gzipped intropolis; specify only when indexing'
+        )
+    parser.add_argument('-x', '--annoy-idx', type=str, required=False,
+            default='jx.idx',
+            help='path to junction index for either writing or reading'
+        )
+    parser.add_argument('--features', type=int, required=False,
+            default=3000,
+            help='dimension of feature space'
+        )
+    parser.add_argument('--n-trees', type=int, required=False,
+            default=200,
+            help='number of annoy trees'
+        )
+    parser.add_argument('--search-k', type=int, required=False,
+            default=None,
+            help='larger = more accurage search'
+        )
+    args = parser.parse_args()
