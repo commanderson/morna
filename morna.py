5e91b1c8f2ae07bd05160a7d5470c227f7fa59b7
Abhi Nellore <anellore@gmail.com>
2017-01-27 16:06:17 -0800

    consolidates index building into MornaIndex class

--- morna.py
+++ morna.py
@@ -10,15 +10,98 @@ Requires mmh3 (pip install mmh3) and Spotify's annoy (pip install annoy)
 """
 import argparse
 import bisect
-import cPickle
 import gzip
-import madoka
+import cPickle
 import mmh3
 import sys
 from annoy import AnnoyIndex
 from collections import defaultdict
 from math import log
 
+class MornaIndex(AnnoyIndex)
+    """ Augments AnnoyIndex to accommodate parameters needed Morna
+
+        Three index files are written when invoking save(): an Annoy index
+        (basename.annoy.mor), a dictionary that maps each junction to the 
+        number of samples in which it's found (basename.freq.mor), and
+        a stats file including the total number of samples.
+
+        See https://github.com/spotify/annoy/blob/master/annoy/__init__.py
+        for original class. 
+    """
+    def __init__(self, sample_count, dim=3000, sample_threshold=100):
+        super(MornaIndex, self).__init__(dim, metric='angular')
+        # Store numbers of samples in which each junction is found
+        self.sample_frequencies = defaultdict(int)
+        # Store low-dimensional representations of samples
+        self.sample_feature_matrix = defaultdict(lambda:
+                                                [0.0 for _ in xrange(dim)]
+                                        )
+        # Dimension of low-dimensional feature feature vector
+        self.dim = self.dimension_count = 3000
+        # Minimum number of samples in which junction should be found
+        self.sample_threshold = sample_threshold
+
+    def add_junction(junction, samples, coverages):
+        """ Adds contributions of a single junction to feature vectors
+
+            junction: string representation of junction
+                (typically chromosome, 1-based inclusive start position,
+                 1-based inclusive end position)
+            samples: list of integer indices of samples
+            coverages: list of integer coverages corresponding to samples
+
+            No return value.
+        """
+        if len(samples) < self.sample_threshold:
+            return
+        self.sample_frequencies[junction] += len(samples)
+        #right now we hash on 'chromosome start stop'
+        #maybe strand someday but shouldn't matter
+        value = mmh3.hash(junction)
+        multiplier = (-1 if value < 0 else 1)
+        hashed_value = hashed_value % args.features
+        idf_value = log(float(num_samples) / self.sample_frequencies[junction])                                           
+        for sample, coverage in zip(samples, coverages):
+            tf_idf_score = (coverage * idf_value)
+            #previously used 1+ norm_log(int(coverage))
+            self.sample_feature_matrix[
+                    int(sample)][
+                    hashed_value] += (multiplier * tf_idf_score)
+
+    def build(self, n_trees, verbose=False):
+        """ Adds sample-feature matrix to Annoy index before building.
+
+            n_trees: number of trees in Annoy index
+            verbose: write status updates to stderr
+
+            No return value.
+        """
+        if verbose:
+            for i, sample in enumerate(self.sample_feature_matrix):
+                self.add_item(sample, sample_feature_matrix[sample])
+                if not (i % 100):
+                    print >>sys.stderr, (
+                            'Added {} samples to Annoy index so far.'
+                        ).format(i+1)
+            print >>sys.stderr, (
+                    'Added a total of {} samples to Annoy index.'
+                ).format(i+1)
+        else:
+            for sample in self.sample_feature_matrix:
+                self.add_item(sample, sample_feature_matrix)
+        super(MornaIndex, self).build(n_trees)
+
+    def save(self, basename):
+        # Save Annoy index first
+        super(MornaIndex, self).save(basename + '.annoy.mor')
+        # Write total number of samples
+        with open(basename + ".stats.mor", 'w') as stats_stream:
+            print >>stats_stream, str(self.sample_count)
+        # Pickle sample frequency dictionary
+        with open(basename + ".freq.mor", 'w') as pickle_stream:
+            cPickle.dump(self.sample_frequencies, pickle_stream,
+                         cPickle.HIGHEST_PROTOCOL)
 
 def parsed_md(md):
     """ Divides an MD string up by boundaries between ^, letters, and numbers
@@ -322,91 +405,62 @@ if __name__ == '__main__':
             default='sam',
             help=('one of {sam, bed, raw}')
         )
-    parser.add_argument('-s','--num-samples', type=int, required=False,
+    parser.add_argument('-s','--sample-count', type=int, required=False,
             default=None,
             help=('optionally specify number of unique sample ids')
+        )
+    parser.add_argument('-t', '--sample-threshold', type=int, required=False,
+            default=100,
+            help='minimum number of samples in which a junction should appear '
+                 'for it to be included in morna index')
+    parser.add_argument('-v', '--verbose', action='store_const', const=True,
+            default=False,
+            help='be talkative'
         )
         
     args = parser.parse_args()
 
     if args.intropolis:
-        #First, sample count to facilitate IDF calculation
-        #list is initialized with a value such that new elements will
-        #ALWAYS sort to the left of it; this eliminates a check for empty list 
-        #from sorted_unique_insert which I'm trying to optimize
-        all_samples = set()
-        
-        num_samples = args.num_samples
-        
-        if not args.num_samples:
-            with gzip.open(args.intropolis) as introp_file_handle:
-                for i, line in enumerate(introp_file_handle):
-                    if (i % 100 == 0):
-                        sys.stdout.write(str(i) + " lines into sample count, " 
-                                + str(len(all_samples)) + " samples so far.\r")
-                    line_pieces = line.split()
-                    samples_with_junction = (line_pieces[6].split(','))
-                    for sample in samples_with_junction:
-                        all_samples.add(sample)
-
-            print("")
-            num_samples = len(all_samples)
-            print("Samples counted:")
-            
-        print("There are " + str(num_samples) + " samples.")
         # Index
-        sample_feature_matrix = defaultdict(
-                                lambda: [0.0 for _ in xrange(args.features)])
-        
-        with open(args.basename + ".stats.mor", 'w') as num_fh:
-            num_fh.write(str(num_samples))
-        
-        junction_sketch = madoka.CroquisUint16()
-        
+        if not args.sample_count:
+            # Count number of samples
+            samples = set()
+            with gzip.open(args.intropolis) as introp_file_handle:
+                if args.verbose:
+                    for i, line in enumerate(introp_file_handle):
+                        if i % 100 == 0:
+                            sys.stdout.write(
+                                str(i) + " lines into sample count, " 
+                                + str(len(all_samples)) + " samples so far.\r"
+                            )
+                        samples.update(line.rpartition('\t')[-1].split(','))
+                else:
+                    for line in enumerate(introp_file_handle):
+                        samples.update(line.rpartition('\t')[-1].split(','))
+            args.sample_count = len(samples)
+            print 'There are {} samples.'.format(args.sample_count)
+        morna_index = MornaIndex(args.sample_count, dim=args.features)
         with gzip.open(args.intropolis) as introp_file_handle:
-            for i, introp_line in enumerate(introp_file_handle):
-                if (i % 100 == 0):
-                    sys.stdout.write(str(i) + " lines into index making\r")
-                introp_line_pieces = introp_line.split()
-                #right now we hash on 'chromosome start stop'
-                #maybe strand someday but shouldn't matter
-                hashable_junction = " ".join(introp_line_pieces[:3])
-                hashed_value = mmh3.hash(hashable_junction)
-                multiplier = (-1 if hashed_value < 0 else 1)
-                hashed_value = hashed_value % args.features
-                samples_junction_coverages = (introp_line_pieces[7].split(','))
-                samples_junction_coverages = [int(num) for num in
-                                             samples_junction_coverages]
-                                     
-                num_samples_with_junction = len(samples_junction_coverages)
-                junction_sketch[hashable_junction
-                                        ] += num_samples_with_junction
-                idf_value = log(float(num_samples)/num_samples_with_junction)
-                                                            
-                for sample_index, coverage in zip(
-                                    introp_line_pieces[6].split(','),
-                                    samples_junction_coverages
-                                ):
-                    tf_idf_score = (coverage * idf_value)
-                    #previously used 1+ norm_log(int(coverage))
-                    sample_feature_matrix[
-                            int(sample_index)][
-                            hashed_value] += (multiplier * tf_idf_score)
-        
-        junction_sketch.save(arg.basename + ".sketch.mor")
-        
-        print("")
-        
-        annoy_index = AnnoyIndex(args.features)
-        i=0;  
-        for sample_index in sample_feature_matrix:
-            if (i % 100 == 0):
-                    sys.stdout.write( str(i) + " samples into annoy building\r")
-            annoy_index.add_item(i,sample_feature_matrix[sample_index])
-            i+=1
-        print("")
-        annoy_index.build(args.n_trees) # n trees specified by args
-        annoy_index.save(args.basename + '.annoy.mor')
+            if args.verbose:
+                for i, line in enumerate(introp_file_handle):
+                    if i % 100 == 0:
+                        sys.stdout.write(str(i) + " lines into index making\r")
+                    tokens = line.strip().split('\t')
+                    morna_index.add_junction(
+                            ' ' .join(tokens[:3]),
+                            map(int, tokens[-2].split(',')), # samples
+                            map(int, tokens[-1].split('',)) # coverages
+                        )
+            else:
+                for line in introp_file_handle:
+                    tokens = line.strip().split('\t')
+                    morna_index.add_junction(
+                            ' ' .join(tokens[:3]),
+                            map(int, tokens[-2].split(',')), # samples
+                            map(int, tokens[-1].split('',)) # coverages
+                        )
+        morna_index.build(args.n_trees, verbose=args.verbose)
+        morna_index.save(args.basename)
     else:
         # Search
         # Read BED or BAM
