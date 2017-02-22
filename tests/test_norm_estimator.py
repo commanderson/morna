fe8113f38df382f3bd6ed6145244437bb20420af
Chris <anderson.christopherc@gmail.com>
2017-02-21 17:03:03 -0800

    test_norm_estimator now has a looping structure, uses a 'random' input vector, and takes a -s argument to support simulation of D. M. Kane and J. Nelson's 'Sparser JL Transform'

--- tests/test_norm_estimator.py
+++ tests/test_norm_estimator.py
@@ -1,5 +1,20 @@
-import random
+import argparse
 import numpy
+import random
+import sys
+from math import sqrt
+
+
+#First set up parser and parse in input file's name.
+parser = argparse.ArgumentParser()
+parser.add_argument('-s', metavar='<int>', type=int,
+            required=False,
+            default=1,
+            help='The number of blocks the hashing space is broken into'
+        )
+        
+args = parser.parse_args()
+
 
 random.seed(8675309)#set seed for reproducible results!
 d= 10000;  #%big dimension
@@ -9,20 +24,6 @@ p = 120;  #%p is dimension of subspace.  m < p < d
 #m = 3000 #we have default dim 3000 for our morna indexes
 #p = 10000 #an estimate for the size of "interesting" subspace
  
-#%create m by d feature hashing matrix Phi
-#Phi = zeros(m,d);
-##phi = [ [0 for _ in range(d)] for _ in range(m)] #want m rows and d columns
-phi = numpy.matrix([0]*(d*m))
-phi.shape = (m,d)
-#for j=1:d
-
-for j in range(d):
-#    I = randperm(m);
-#    Phi(I(1),j) = sign(rand-.5);
-	spot=random.randint(0,m-1) #randint is inclusive; avoid index out of bounds!
-	##phi[spot][j] = random.choice((-1,1))
-	phi[spot,j] = random.choice((-1,1))
-#end
 
 
 #%create d-dimensional test vector (normalized so that norm(x,2) = 1)
@@ -36,42 +37,87 @@ for idx in range(p,d):
 	x[idx,0]=.1
 #x = x/norm(x,2);
 
+x = numpy.matrix([random.uniform(1.0,1000.0) for _ in range(d)] )
+x = x.transpose()
+
 x=x/numpy.linalg.norm(x,2)
  
-#% y is feature hashing of x.  norm(y,2) should be rougly equal to
-#% norm(x,2)
-#y = Phi*x;
-y = numpy.matmul(phi,x)
-print("norm of y is " + str(numpy.linalg.norm(y,2)))
-print("norm of x is " + str(numpy.linalg.norm(x,2)))
+real_values = []
+estimations = []
+blocksize = m/args.s
 
-#norm(y,2);
+for loop in range(10):
+	if not (loop % 100):
+		sys.stdout.write("Loop %d\r" % loop)
+	#%create m by d feature hashing matrix Phi
+	#Phi = zeros(m,d);
+	##phi = [ [0 for _ in range(d)] for _ in range(m)]
+	#want m rows and d columns
+	phi = numpy.matrix([0]*(d*m))
+	phi.shape = (m,d)
+	#for j=1:d
+
+	for j in range(d):
+		for block in range(args.s):
+			print("Block %d is from %d to %d" %
+					 (block,block*blocksize,((block+1)*blocksize)-1))
+			spot=random.randint(block*blocksize,((block+1)*blocksize)-1) 
+			#randint is inclusive; avoid index out of bounds!
+			##phi[spot][j] = random.choice((-1,1))
+			phi[spot,j] = random.choice((-1,1))
+	#end
+
+	#% y is feature hashing of x.  norm(y,2) should be rougly equal to
+	#% norm(x,2)
+	#y = Phi*x;
+	y = numpy.matmul(phi,x)
+	#print("norm of y is " + str(numpy.linalg.norm(y,2)))
+	#print("norm of x is " + str(numpy.linalg.norm(x,2)))
+
+	#norm(y,2);
  
+	#%e1 is a biased estimator for norm(x(1:p),2);
+	#%I believe that E[e1^2] = ((m-1)/m) norm(x(1:p,2),2)^2 + p/m
+	#z = Phi(:,1:d)'*y; #'* aka transpose (') and then matmult (*) each of the columns of phi with y
+	#is equivalent to dot product for column vector for each of these
+	z = numpy.matrix([0.0 for _ in range(d)] )
+	for column_num in range(d):
+		z[0,column_num] = (phi[:,column_num].transpose() * y )
+	#e1 = norm(z(1:p,1),2)
+	e1 = numpy.linalg.norm(z[0,range(0,p)],2)
  
-#%e1 is a biased estimator for norm(x(1:p),2);
-#%I believe that E[e1^2] = ((m-1)/m) norm(x(1:p,2),2)^2 + p/m
-#z = Phi(:,1:d)'*y; #'* aka transpose (') and then matmult (*) each of the columns of phi with y
-#is equivalent to dot product for column vector for each of these
-z = numpy.matrix([0.0 for _ in range(d)] )
-for column_num in range(d):
-	z[0,column_num] = (phi[:,column_num].transpose() * y )
-#e1 = norm(z(1:p,1),2)
-e1 = numpy.linalg.norm(z[0,range(0,p)],2)
+	#%Thus using that norm(x,2)^2 = 1, 
+	#%I suggest exploring e2 = sqrt( max{0,e1^2 - p/m}) 
+	#%as a nearly unbiased
+	#%proxy for norm(x(1:p),2)
  
-#%Thus using that norm(x,2)^2 = 1, 
-#%I suggest exploring e2 = sqrt( max{0,e1^2 - p/m}) 
-#%as a nearly unbiased
-#%proxy for norm(x(1:p),2)
- 
-#if e1^2 > p/m
-#    e2 = sqrt((m/(m-1))*(e1^2 - p/m));
-#else
-#    e2 = 0;
-#end
-e2 = sqrt( max(0,e1**2 - float(p)/m))
- 
-#%Compare 
-#norm(x(1:p),2)  
-#e2
-print("True Norm: " + str(numpy.linalg.norm(x[range(p)])))
-print("e2 Estimate: " + str(e2))
+	#if e1^2 > p/m
+	#    e2 = sqrt((m/(m-1))*(e1^2 - p/m));
+	#else
+	#    e2 = 0;
+	#end
+
+	#e2 = sqrt( max(0,(float(m)/(m-1))*(e1**2 - float(p)/m)))
+	e2 = sqrt( max(0,(e1**2 - float(p)/m)))
+	#%Compare 
+	#norm(x(1:p),2)  
+	#e2
+
+
+	#print("True Norm: " + str(numpy.linalg.norm(x[range(p)])))
+	real_values.append(numpy.linalg.norm(x[range(p)]))
+	#print("e2 Estimate: " + str(e2))
+	estimations.append(e2)
+	#print("Ratio TN/e2: " + str(numpy.linalg.norm(x[range(p)])/e2))
+
+sys.stdout.write("\n")
+differences = [real_values[i]-estimations[i] for i,_ in enumerate(estimations)]	
+e2_rv_ratio = [estimations[i]/real_values[i] for i,_ in enumerate(estimations)]	
+
+print("mean real value:" + str(numpy.mean(real_values)))
+#print("real value variance:" + str(numpy.var(real_values)))
+print("mean estimation:" + str(numpy.mean(estimations)))
+print("estimation variance:" + str(numpy.var(estimations)))
+print("mean difference:" + str(numpy.mean(differences)))
+print("difference variance:" + str(numpy.var(differences)))
+print("mean e2/rv ratio:" + str(numpy.mean(e2_rv_ratio)))
