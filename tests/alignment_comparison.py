d1b47ae70c6b5b0c99f2145b00ebfa17753e42c8
Chris <anderson.christopherc@gmail.com>
2017-06-05 18:04:50 -0700

    adds to alignment comparison beginnings of necessary structure for actual comparison of highest scoring alignments but does not complete (unfinshed state)

--- tests/alignment_comparison.py
+++ tests/alignment_comparison.py
@@ -4,7 +4,10 @@ alignment_comparison
 
 compare a list of .bam alignment files with a master .bam alignment
 to determine what percentage of reads in each listed file have an identical
-mapping (coordinate + cigar string) in the master file
+highest-scoring mapping (coordinate + cigar string) in the master file
+
+Note the concept of fractional mappings - if master file has 4 equally high scoring mappings, and 3 of these are present in a compared file, then .75 of an identical mapping is present for that read in that file.
+
 """
 import argparse
 import subprocess
@@ -13,7 +16,9 @@ from itertools import groupby
 
 _help_intro = \
 """alignment_comparison determines what portion of reads in each of a list of bam files have an identical mapping (coordinate + cigar string) present in a
-master bam file.
+master bam file. 
+Requires that reads be sorted by read name aka the QNAME field-
+use samtools sort -n to get these
 """
 
 if __name__ == '__main__':
@@ -28,56 +33,65 @@ if __name__ == '__main__':
             required=False,
             help='paths to .bam alignment files to evaluate'
         )
-    
+    parser.add_argument('-v', '--verbose', action='store_const',
+            const=True,
+            default=False,
+            help='be talkative'
+        )
+        
     args = parser.parse_args()
     
     read_dict = {}
     process1 = subprocess.Popen(["samtools","view","-F","4",
                                  args.master], stdout=subprocess.PIPE)
+                                 
+    sub_iterators = []
+    current_read_groups = []
+    for file in args.subfiles:
+        process_sub = subprocess.Popen(["samtools","view","-F","4",
+                                     file], stdout=subprocess.PIPE)
+        sub_iterators.append(groupby(iter(process_sub.stdout.readline, ''),
+                                             lambda x: x.split()[0]))
+    for iterator in sub_iterators:
+        current_read_groups.append(next(iterator))
+        
     for name, group in groupby(iter(process1.stdout.readline, ''),
                                              lambda x: x.split()[0]):
-        max_alignment_score=0
-        for read in group:
-            #Is it a max scoring alignment? 
-            #If replaces current, new list
+        max_alignment_score=-99999999999
+        alignments_list = []
+        for alignment in group:
+            fields = alignment.split()
+            score = int(fields[11].strip("AS:i"))
+            #Is it a max scoring alignment?
+            if score > max_alignment_score:
+                max_alignment_score = score
+                alignments_list = [(fields[0],fields[3],fields[5],)]
             #if equals current, add to list
+            elif score == max_alignment_score:
+                alignments_list.append((fields[0],fields[3],fields[5],))
             #else do nothing
-            print read.split()[:3]
-        print "-------------------------"
-
-    for i,line in enumerate(iter(process1.stdout.readline, '')):
-        if (i%100 == 0):
-            sys.stdout.write(str(i) + " lines into master file processing\r")
-        fields = line.split()
-        #name = (fields[0],fields[1])
-        #rep = (fields[2],fields[3],fields[5])
-        #The "key" which uniquely represents a read is the SAM fields 
-        #QNAME (query template name)
-        #FLAG (bitwise flag about the mapping)
-        #RNAME (reference sequence name)
-        #A given combination of these uniquely represents a specific read
-        #The mapping of that read is represented in the SAM fields
-        #POS (1-based leftmost mapping position of read)
-        #CIGAR (details of exact base mapping)
-        #If two reads have identical POS and CIGAR fields, 
-        #they have mapped identically.
-        read_dict[(fields[0],fields[1],fields[2])] = (fields[3],fields[5])
         
-    for file in args.subfiles:
-        sys.stdout.write("\n")
-        process2 = subprocess.Popen(["samtools","view","-F","4",
-                                     args.master], stdout=subprocess.PIPE)
-        num_mapped_reads = 0
-        num_identical_mappings = 0
-        for i,line in enumerate(iter(process2.stdout.readline, '')):
-            if (i%100 == 0):
-                sys.stdout.write(str(i) + " lines into " + file 
-                                                        + " processing\r")
-            num_mapped_reads += 1
-            fields = line.split()
-            if read_dict[(fields[0],fields[1],fields[2])] == (fields[3],
-                                                                fields[5]):
-                num_identical_mappings += 1
-        print i
-        print("For file " + file + " there are " + str(num_mapped_reads) + " mapped reads, " + str(num_identical_mappings) + " identical (" + str(float(num_identical_mappings)/num_mapped_reads) + ")")
-    
+        if args.verbose:
+            print "--------------------------------------------------"
+            sys.stdout.write("Alignments in this group:\n")
+            for entry in alignments_list:
+                sys.stdout.write(str(entry) + "\n")
+            sys.stdout.write("Max score: " + str(max_alignment_score) + "\n")
+        for i,file in enumerate(args.subfiles):
+            #if the current group from that file has the same read name
+            if current_read_groups[i][0] == name:
+                #then we need to look at the alignments in that group in subfile
+                #to see how many from our list are matched
+                #TODO: FIND HIGHEST SCORING LIST HERE TOO?
+                matching = 0.0
+                for sub_alignment in current_read_groups[i][1]:
+                    sub_fields = sub_alignment.split()
+                    if (sub_fields[0],sub_fields[3],
+                                        sub_fields[5],) in alignments_list:
+                        matching += 1
+                matching /= 
+                    
+                
+        #for name, group in groupby(iter(process2.stdout.readline, ''),
+        #                                     lambda x: x.split()[0]):
+        
